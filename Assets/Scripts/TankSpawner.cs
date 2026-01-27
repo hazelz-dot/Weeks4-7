8844f12ab2cb2198643df6dcf965136ce205f98c
DESKTOP-S6JHD3R\ZhanO <hazel.zhou0966@gmail.com>
2026-01-27 01:02:53 -0500

    edited spawner

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -13,9 +13,22 @@ public class TankSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current.leftButton.wasPressedThisFrame == true)
         {
-            Instantiate(tankPrefab);
+            //instantiate a prefab: make this one appear
+            //instantiate a prefab, a vector2 or vector3, a quaternion: make it appear at this position and rotation
+            //Instantiate(tankPrefab);
+            //Instantiate(tankPrefab, transform.position, transform.rotation);
+
+            Vector2 spawnPos = Random.insideUnitCircle * 3;
+            //quaternion.identity means no rotation which is the same as euler angles (0,0,0)
+            Instantiate(tankPrefab, spawnPos, Quaternion.identity);
+        }
+
+        if (Mouse.current.rightButton.wasPressedThisFrame = true)
+        {
+            //instantiate a prefab, a transform: make it appear at 0,0, as the child of that transform
+            Instantiate(tankPrefab, transform);
         }
     }
 }
